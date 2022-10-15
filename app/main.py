5ab300154dcbb8a9882a2741db23d4b39cd682f3
IlyaMazaev <ilya9291@yandex.com>
2022-10-15 15:47:07 +0300

    added route /video

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from model.inference import get_results
 from app.database.database import global_init
 from app.routers import stone, frame
 
@@ -25,5 +24,4 @@ async def init_database_on_startup():
 
 @app.get("/")
 async def healthcheck_index():
-    for i in get_results('video.mp4'):
-        return {"success": i}
+    return {'success': 'ok'}
--- app/routers/frame.py
+++ app/routers/frame.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 
 from app.database.models.frame import Frame, PydanticFrame
 from app.dependencies import auth, database
+from model.inference import get_results
 
 from pydantic import ValidationError
 
@@ -17,3 +18,9 @@ async def get_all_frames(session=Depends(database.session)) -> list:
 @router.get("/{frame_id}")
 async def get_singe_frame_data(frame_id: int, session=Depends(database.session)) -> PydanticFrame:
     return Frame.get_by_frame_id(session, frame_id)
+
+@router.get("/video/")
+async def get_video_stream(session=Depends(database.session)):
+    for i in get_results('video.mp4'):
+        print(i)
+
